45f563c6332c475b8c5d68a863f5941383027e61
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2024-11-18 10:54:46 +0530

    Module 6 Lecture 6 18-11

--- Tailwind/Lecture-5/installation/tailwind.config.js
+++ Tailwind/Lecture-5/installation/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        hitesh: '#389eea',
+        akshara: '#cb00f5',
+      },
+      spacing: {
+        100: '100px',
+      },
+      boxShadow: {
+        pinklines:
+          'rgba(240, 46, 170, 0.4) 5px 5px, rgba(240, 46, 170, 0.3) 10px 10px, rgba(240, 46, 170, 0.2) 15px 15px, rgba(240, 46, 170, 0.1) 20px 20px, rgba(240, 46, 170, 0.05) 25px 25px;',
+      },
+    },
   },
   plugins: [],
 };
